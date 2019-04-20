8c18912ae69bc75b9448f948c0f223c58b5cd7de
mohab amr <mohabamr1@gmail.com>
2019-04-20 16:46:03 +0200

    removed invalid line 3abeeet

--- main.py
+++ main.py
@@ -272,7 +272,6 @@ def get_token_type(token):
 
 def main():
     output_file = open('nim_result.txt', 'w+')
-    output_file.write("invalid")
     # try:
     with open(args.file, "r") as file:
         lines = file.read()
